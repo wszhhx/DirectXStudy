ca75e1d9eb07ad6820294b48db9bff3466c645c8
hershel.z <hershel.z@mechanist.co>
2021-03-24 18:10:43 +0800

    建立框架

--- DirectXFreshMan/源.cpp
+++ DirectXFreshMan/源.cpp
@@ -1,15 +1,13 @@
-#include<d3d10shader.h>
-#include<d3d.h>
 #include<d3d10.h>
-#include<d3d10misc.h>
-#include<d3d10sdklayers.h>
-#include<d3d10effect.h>
-#include<iostream>
 
 using namespace std;
 
 
 
+
 int main() {
+	ID3D10VertexShader* BasicShader = 0;
+	ID3D10
 	
+
 }
