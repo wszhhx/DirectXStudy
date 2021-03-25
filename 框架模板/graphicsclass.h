56e46994459caa5487524f4b628fdb653ff3199f
hershel.z <hershel.z@mechanist.co>
2021-03-25 17:02:49 +0800

    基础框架搭建完毕

---
+++ 框架模板/graphicsclass.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+#include<windows.h>
+
+///
+//全局变量//
+///
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+
+class GraphicsClass {
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+};
+
+#endif</windows.h> // !1
